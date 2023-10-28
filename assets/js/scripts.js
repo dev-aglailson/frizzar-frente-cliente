a016bb6f22156632f934181cc491637f24486e26
joseaglailson <jose.aglailson@wapstore.com.br>
2023-10-28 12:55:50 -0300

    storys finalizados

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    const swiperTimelinePassos = new Swiper('.banner', {
+    const swiperBanner = new Swiper('.banner', {
         loop:true,
         effect:'slide',
         autoplay: {
@@ -11,6 +11,34 @@ $(document).ready(function(){
             enabled:false,
         },
         speed:1000,
+
+        
+    });
+
+    const swiperStory = new Swiper('.slide-story', {
+        loop:false,
+        effect:'slide',
+        margin:0,
+        autoplay: {
+            delay: 300,
+            disableOnInteraction:false,
+            pauseOnMouseEnter:true,
+            waitForTransition:true,
+            enabled:false,
+        },
+        speed:1000,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 4,
+              spaceBetween: 0
+            },
+            // when window width is >= 480px
+            680: {
+              slidesPerView: 6,
+              spaceBetween: 30
+            },
+        },
 
         
     });
