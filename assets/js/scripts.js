6f68d19b4ff6510a67bcc1130fed09d55a8349f2
joseaglailson <jose.aglailson@wapstore.com.br>
2023-10-28 18:31:52 -0300

    Animação contato funcionando

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var btnsStories = document.querySelectorAll('.btn-storie')
     var slideStory  = document.querySelector('.box-story-grande-slide')
     var slideStoryAberto = false
+    var btnToggleDias = document.querySelector('.btb-toggle-dias')
 
     btnsStories.forEach(btn=>{
         btn.addEventListener('click', function(){
@@ -32,6 +33,27 @@ $(document).ready(function(){
         }
 
     })
+
+    btnToggleDias.addEventListener('click', function(){
+        toggleDiasFuncionamento(this)
+    })
+
+    function toggleDiasFuncionamento(btnToggleDias=null){
+
+        let divDias = btnToggleDias.nextElementSibling
+        //Torna a altura no formato boolean para verifcar se tem altura ou não
+        let alturaDivDias = !!divDias.style.maxHeight
+
+        if(alturaDivDias){
+            divDias.style.maxHeight = null
+            btnToggleDias.querySelector('.icon-btn-funcionamento').classList.remove('icon-btn-funcionamento-open')
+        }else{
+
+            divDias.style.maxHeight = divDias.scrollHeight+"px"
+            btnToggleDias.querySelector('.icon-btn-funcionamento').classList.add('icon-btn-funcionamento-open')
+        }
+
+    }
     
     const swiperBanner = new Swiper('.banner', {
         loop:true,
@@ -73,7 +95,7 @@ $(document).ready(function(){
         breakpoints: {
             // when window width is >= 320px
             320: {
-              slidesPerView: 4,
+              slidesPerView: 6,
               spaceBetween: 0
             },
             // when window width is >= 480px
